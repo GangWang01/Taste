2b9aa91417a64a0b38e76d0fac2926357d7b5d3d
Gang Wang <v-gaw@microsoft.com>
2024-04-09 10:23:24 +0000

    Change the rule

--- eng/update_version.sh
+++ eng/update_version.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Function to get the current git branch name
+get_git_branch() {
+    git rev-parse --abbrev-ref HEAD
+}
+
+# Call the function and store the result
+branch_name=$(get_git_branch)
+if [[ "$branch_name" =~ "^rel/[0-9]+\.[0-9]+$" ]]; then
+
 # Read the current version from the file
 versionFile="./eng/Versions.props"
 currentVersion=$(grep -oP "<VersionPrefix>\K(.*)(?=</VersionPrefix>)" "$versionFile" | sed 's/\s*//')
@@ -17,3 +26,6 @@ echo "The VersionPrefix in $versionFile has been bumped up from $currentVersion 
 
 # Add the updated version file to the staging area
 git add "$versionFile"
+
+fi
+
